f5631a56a93e732705f5f9317a67b92827fd59c1
Joanthan Gross <jonathan.gross@biodata.com>
2018-01-16 12:51:58 +0200

    moving to farady

--- lib/labguru.rb
+++ lib/labguru.rb
@@ -4,66 +4,79 @@ require 'json'
 class Labguru
  API_URL = "https://my.labguru.com" 
  def initialize(email,password)
-   @base = "https://api.labguru.com"
+   @base = API_URL
    @email = email 
    @password = password
-   @token = login(@email, @password)
+ end
+
+ def login(email = @email, password = @password)
+   @token = do_login(email, password)
+ end
+
+ def server=(base)
+   @base = base
  end
 
  def token 
  	return @token
  end
  #returns a token or -1 for null vallues. 
- def login(email = "",password = "")
+ def do_login(email = "",password = "")
    endpoint = "/api/v1/sessions.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {login: email, password: password})
+   response = Faraday.post("#{@base}#{endpoint}", {login: email, password: password})
    json = JSON.parse(response.body)
    return json["token"]
  end 
 
  def add_project(name, description = "")
    endpoint = "/api/v1/projects.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {item:{title: name, description: description}, token:@token})
+   response = Faraday.post("#{@base}#{endpoint}", {item:{title: name, description: description}, token:@token})
    return_id(response.body, "Projects::Project")
  end 
 
- def add_folder(project_id,name,description = "")
+ def add_folder(project,name,description = "")
    endpoint = "/api/v1/milestones.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {item:{project_id: project_id, title: name, description: description}, token:@token})
+   response = Faraday.post("#{@base}#{endpoint}", {item:{project_id: project['id'], title: name, description: description}, token:@token})
    return_id(response.body,"Projects::Milestone")
  end 
  
- def add_experiment(project_id,folder_id,name)
+ def add_experiment(project,folder,name)
    endpoint = "/api/v1/experiments.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {item:{project_id: project_id, milestone_id: folder_id, title: name}, token:@token})
+   response = Faraday.post("#{@base}#{endpoint}", {item:{project_id: project['id'], milestone_id: folder['id'], title: name}, token:@token})
    return_id(response.body, "Projects::Experiment")
  end
 
- def add_section(experiment_id,name,position = 999)
+ def add_section(container,name,position = 999)
   endpoint = "/api/v1/element_containers.json"
-  response = Faraday.post("#{API_URL}#{endpoint}", {item:{container_type: "Projects::Experiment" ,container_id: experiment_id, name: name,position: position}, token:@token})
+  response = Faraday.post("#{@base}#{endpoint}", {item:{container_type: "Projects::Experiment" ,container_id: container['id'], name: name,position: position}, token:@token})
   return_id(response.body,"ExperimentProcedure")
  end 
 
- def add_element(section_id, type = "text" , data)
+ def add_element(container, type = "text" , data)
    endpoint = "/api/v1/elements.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {item:{container_type: "ExperimentProcedure" ,container_id: section_id, element_type: type, data: data}, token:@token})
+   response = Faraday.post("#{@base}#{endpoint}", {item:{container_type: "ExperimentProcedure" ,container_id: container['id'], element_type: type, data: data}, token:@token})
    return_id(response.body,"Element")
  end
 
  def add_paper(pmid)
-   url = "https://my.labguru.com/knowledge/papers/p#{pmid}.json?token=#{@token}"
+   url = "#{@base}/knowledge/papers/p#{pmid}/add_pubmed.json?token=#{@token}"
    response = Faraday.get(url)
    return_id(response.body,"Knowledgebase::Paper")
  end 
 
- def add_attachment(object_id,object_type, file_name )
-
+ def add_attachment(object, file_path, name, description )
+    endpoint = "/api/v1/attachments.json"
+    response = Faraday.post("#{@base}#{endpoint}", {item:{ 
+      title: name,
+      description: description,
+      attachment: File.open(path,"r"),
+      attach_to_uuid: object['uuid']}, token:@token})
+    return_id(response.body,"System::Attachment")
  end
 
  def add_link(source_id, source_class, target_id, target_class)
    endpoint = "/api/v1/links.json"
-   response = Faraday.post("#{API_URL}#{endpoint}", {item:{source_type: source_class, source_id: source_id, target_type: target_class, target_id: target_id}, token:@token})
+   response = Faraday.post("#{@base}#{endpoint}", {item:{source_type: source_class, source_id: source_id, target_type: target_class, target_id: target_id}, token:@token})
    return_id(response.body, "Link")
  end
  
