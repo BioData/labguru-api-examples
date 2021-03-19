57a17863e42d915ff299438fbf11c2cb3d5c4ade
Joanthan Gross <jonathan.gross@biodata.com>
2021-03-19 10:02:43 +0200

    working on screen

---
+++ python_examples/screen/base.py
@@ -0,0 +1,95 @@
+import requests
+import os
+import json
+
+BASE_URL = 'https://my.labguru.com/api/v1'
+TOKEN = ""
+def login(username,password):
+  url = BASE_URL + "/sessions.json"
+  data = {"login": username,"password": password}
+  resp = requests.post(url, json=data)
+  return resp.json()['token']
+
+def add_project(name):
+  url = BASE_URL + "/projects.json"
+  data = {"item": {"title": name},"token": TOKEN}
+  project = requests.post(url, json=data)
+  return project.json()
+
+def add_folder(project_id,name):
+  url = BASE_URL + "/milestones.json"
+  data = {"item": {"project_id": project_id,"title": name},"token": TOKEN}
+  folder = requests.post(url, json=data)
+  return folder.json()
+
+def add_experiment(project_id,folder_id,name):
+  url = BASE_URL + "/experiments.json"
+  data = {"item": {"project_id": project_id, "milestone_id": folder_id, "title": name},"token": TOKEN}
+  experiment = requests.post(url, json=data)
+  return experiment.json()
+
+def add_section(experiment_id, name):
+  url = BASE_URL + "/sections.json"
+  data = {"item": {"experiment_id": experiment_id, "name": name, "container_id": experiment_id, "container_type": "Projects::Experiment"},"token": TOKEN}
+  section = requests.post(url, json=data)
+  return section.json()
+
+def add_empty_plate(experiment_id,section_id,cols,rows,name):
+  url = BASE_URL + "/elements.json"
+  data ={
+	"item":  {
+    "name": name,
+    "element_type": "plate",
+    "container_type": "ExperimentProcedure",
+    "container_id": section_id,
+    "rows": rows,
+    "cols": cols,
+    "experiment_id": experiment_id
+    },"token": TOKEN
+  }
+  element = requests.post(url, json=data)
+  return element.json()
+
+def add_layer_data_to_plate(plate_id,filepath):
+  url = BASE_URL + "/plates/" + str(plate_id) + "/update_layer?token=" + TOKEN
+  print(url)
+  f = open(filepath, "rb")
+  response = requests.put(url, files= {"file": f})
+  return response
+
+def convert_plate_xls_to_json(xlsx_filepath,container_id):
+  url = BASE_URL + "/elements/convert_xlsx_to_json?token=" + TOKEN + "&exp_pro_container_id=" + str(container_id)
+  print(url)
+  f = open(xlsx_filepath, "rb")
+  response = requests.post(url, files= {"file_name[0]": f})
+  #update plate data with the response
+  print(response)
+  plate_data = response.json()
+  plate_json = json.dumps(plate_data["data"])
+  return plate_json
+
+def update_plate(plate_id,plate_data):
+  url = BASE_URL + "/elements/" + str(plate_id)
+  print(url)
+  response = requests.put(url, json=plate_data)
+  return response.json()
+
+def download_plate_xlsx(plate_id):
+  url = BASE_URL + "/elements/" + str(plate_id) + "/download_plate_xlsx.json?token=" + TOKEN
+  response = requests.get(url)
+
+def clone_plate(plate_id,names):
+  url = BASE_URL + "/plates/" + str(plate_id) + "/duplicate?token=" + TOKEN
+  print(url)
+  response = requests.post(url, json = {"names": ["A","B","C"]})
+  print(response.status_code)
+  data = response.json()
+  return data
+
+def register(klass,filepath):
+  print("TBD")
+  return [] #to be implementd
+
+def add_sample_element(a,b,c):
+  print("TBD")
+  return [] #to be implementd
---
+++ python_examples/screen/screen.py
@@ -0,0 +1,62 @@
+import pdb
+import os
+import base as lg
+
+lg.BASE_URL = "http://localhost:3000/api/v1"
+lg.TOKEN = lg.login(os.environ.get("LGEMAIL"), os.environ.get("LGPASSWORD"))
+# EXAMPLE ONLY - DATA is not real and has no impact
+# This experiment is going to test the impact of 10 differnt sugers on cell growth
+# We are going to grow cells in 10, 384 well plates
+# Each plate will have 1 type of compound (complex suger)
+# With differnt conenteration and controls
+# Each plate / well will be images 2 times (after 6 hours and after 20 hours)
+
+#0. set up a project
+project_id    = lg.add_project("Impact of Sugar concentration on Cell Growth")['id']
+folder_id     = lg.add_folder(project_id,"Master Plate Design")['id']
+experiment_id = lg.add_experiment(project_id,folder_id,"Master Plate")['id']
+section_id    = lg.add_section(experiment_id,"Master Plate")['id']
+
+#register 10 differnt compounds via SDF import
+sugars          = lg.register("Chemistry::Compound","data/sugars.sdf")
+
+#add the cell lines needs for this experiment
+cell_ids         = lg.register("Biocollection::CellLine", "data/cell_line.xlsx")
+
+#1. add an empty plate
+plate_id      = lg.add_empty_plate(experiment_id,section_id,24,16,"Master Plate")['id']
+
+#2. add cells to a sample element
+sample_element = lg.add_sample_element(section_id,cell_ids,"Biocollection::CellLine")
+
+#3. add cells to this plate
+plate_json    = lg.add_layer_data_to_plate(plate_id,'data/plate_default_cells.csv')
+
+#4. add meta data layer to the plate - declare controls and borders ;
+plate_json    = lg.add_layer_data_to_plate(plate_id,'data/plate_default_meta.csv')
+
+#5. utility - get plate json from xlsx
+xlsx_file    = lg.download_plate_xlsx(plate_id)
+plate_data   = lg.convert_plate_xls_to_json(xlsx_file,section_id)
+
+#6. create another empty plate and populate it with plate data
+plate_json    = lg.add_empty_plate(experiment_id,section_id,24,16,"Base Replica Plate")
+new_plate_id  = plate_json["id"]
+plate_json["data"] = plate_data
+plate_data    = lg.update_plate(new_plate_id,plate_json)
+
+#7. Replicate Base plates
+names         = ["Replica A", "Replica B"]
+replicates    = lg.clone_plate(plate_id,names)
+
+#8. Createa a filter to display only plates related to this experiment
+#search = add_search()
+#9. Add this filter to the experiment page
+
+#10. Per plate attach an image
+
+#11. Per image - run an image analysis workflow
+
+#12. Add data per well
+
+#13. Add plate datasets
