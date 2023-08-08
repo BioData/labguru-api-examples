326f917aea17b001649e0b95b107830676851d72
Roy Gal <roy.gal@biodata.com>
2023-08-08 17:05:41 +0300

    feat(S3DirectUpload) LAB-20184: Add required params and error handling

--- python_examples/s3_direct_upload.py
+++ python_examples/s3_direct_upload.py
@@ -17,7 +17,8 @@ API_BASE = "SERVER"
 LOGIN = "EMAIL"
 PASSWORD = "PASSWORD"
 CONTENT_TYPE = "application/octet-stream"
-
+UUID = "UUID"
+ATTACHABLE_TYPE = "TYPE"
 # get your token
 try:
     token_request = requests.post(
@@ -38,21 +39,27 @@ try:
                 "original_filename": filename,
                 "content_type": CONTENT_TYPE,
             },
+            "attach_to_uuid": UUID,
+            "attachable_type": ATTACHABLE_TYPE,
         },
         "token": token,
     }
 
-    attachment_request = requests.post(
-        os.path.join(API_BASE, "api/v2/attachments/direct_upload"), json=data
+    attachment_response = requests.post(
+        os.path.join(API_BASE, "api/v2/attachments/s3_direct_upload"), json=data
     )
 
-    attachment_request.raise_for_status()
+    attachment_response.raise_for_status()
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
 
-print(attachment_request.status_code)
-json = attachment_request.json()
+print(attachment_response.status_code)
+if (attachment_response.status_code != 200):
+    print(attachment_response.content)
+    exit(1)
+
+json = attachment_response.json()
 url = json["presigned_url"]
 data_to_upload = open(path, "rb")  # Open the file in binary mode
 upload_response = requests.put(url, data=data_to_upload)
